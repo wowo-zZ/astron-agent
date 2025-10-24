603e65fa4b03aded96002a9c2dc73778537e80f5
7bs2 <sunflower.7bs2@gmail.com>
2025-10-24 09:26:46 +0800

    feat：voice config

---
+++ console/backend/hub/src/main/java/com/iflytek/astron/console/hub/service/bot/impl/VoiceServiceImpl.java
@@ -0,0 +1,70 @@
+package com.iflytek.astron.console.hub.service.bot.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.iflytek.astron.console.commons.constant.ResponseEnum;
+import com.iflytek.astron.console.commons.exception.BusinessException;
+import com.iflytek.astron.console.hub.entity.PronunciationPersonConfig;
+import com.iflytek.astron.console.hub.mapper.PronunciationPersonConfigMapper;
+import com.iflytek.astron.console.hub.service.bot.VoiceService;
+import com.iflytek.astron.console.toolkit.tool.http.HeaderAuthHttpTool;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author bowang
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class VoiceServiceImpl implements VoiceService {
+
+    @Value("${spark.app-id}")
+    private String appId;
+
+    @Value("${spark.api-key}")
+    private String apiKey;
+
+    @Value("${spark.api-secret}")
+    private String apiSecret;
+
+    @Value("${spark.tts-api-url}")
+    private String ttsApiUrl;
+
+    private final PronunciationPersonConfigMapper pronunciationPersonConfigMapper;
+
+    @Override
+    public Map<String, String> getTtsSign() {
+        try {
+            Map<String, String> resultMap = new HashMap<>();
+            String url = HeaderAuthHttpTool.get(ttsApiUrl, apiKey, apiSecret);
+            resultMap.put("appId", appId);
+            resultMap.put("apiKey", apiKey);
+            resultMap.put("apiSecret", apiSecret);
+            resultMap.put("url", url);
+            return resultMap;
+        } catch (NoSuchAlgorithmException | IOException | InvalidKeyException e) {
+            log.error("get tts sign error:{}", e.getMessage());
+            throw new BusinessException(ResponseEnum.BUSINESS_ERROR);
+        }
+    }
+
+    @Override
+    @Cacheable(value = "pronunciationPersonCache", key = "#root.methodName", cacheManager = "cacheManager5min")
+    public List<PronunciationPersonConfig> getPronunciationPerson() {
+        LambdaQueryWrapper<PronunciationPersonConfig> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(PronunciationPersonConfig::getModelManufacturer, PronunciationPersonConfig.ModelManufacturerEnum.XFYUN);
+        queryWrapper.eq(PronunciationPersonConfig::getDeleted, 0);
+        queryWrapper.orderByAsc(PronunciationPersonConfig::getSort);
+        return pronunciationPersonConfigMapper.selectList(queryWrapper);
+    }
+}
