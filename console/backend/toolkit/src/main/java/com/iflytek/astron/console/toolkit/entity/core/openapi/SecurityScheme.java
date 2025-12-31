c711366ee0befe8f72d817b959712f4b65bc8244
yingpeng <yingpeng@iflytek.com>
2025-12-31 09:47:15 +0800

    feat(openapi): add value field to SecurityScheme and set token in ToolBoxService

--- console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/entity/core/openapi/SecurityScheme.java
+++ console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/entity/core/openapi/SecurityScheme.java
@@ -1,5 +1,6 @@
 package com.iflytek.astron.console.toolkit.entity.core.openapi;
 
+import com.alibaba.fastjson2.annotation.JSONField;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,6 @@ public class SecurityScheme {
     String type;
     String name;
     String in;
+    @JSONField(name = "x-value")
+    String value;
 }
